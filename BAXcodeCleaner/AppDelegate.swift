013451a3b7ceeaacce7b557b2d167d283d9f1eef
arvinnie <arvinnie@tencent.com>
2019-12-25 11:39:54 +0800

    feat: first commit

---
+++ BAXcodeCleaner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BAXcodeCleaner
+//
+//  Created by BenArvin on 2019/12/25.
+//  Copyright © 2019 BenArvin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BAXcodeCleaner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BAXcodeCleaner
+//
+//  Created by BenArvin on 2019/12/25.
+//  Copyright © 2019 BenArvin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
